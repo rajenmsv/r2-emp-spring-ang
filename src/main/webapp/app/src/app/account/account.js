ec964dbb7fe2ce8bada3397c544c49c303f20308
sanjay varma <sanjayvarma.b@gmail.com>
2015-09-27 12:00:56 -0400

    angular js - 12th video chris henkel's javascript setup

--- src/main/webapp/app/src/app/account/account.js
+++ src/main/webapp/app/src/app/account/account.js
@@ -1,86 +1,113 @@
-angular.module('ngBoilerplate.account', ['ui.router','ngResource'])
-    .config(function($stateProvider) {
-        $stateProvider.state('login', {
-            url:'/login',
+angular.module('ngBoilerplate.account', ['ui.router', 'ngResource', 'base64'])
+.config(function($stateProvider) {
+    $stateProvider.state('login', {
+        url:'/login',
+        views: {
+            'main': {
+                templateUrl:'account/login.tpl.html',
+                controller: 'LoginCtrl'
+            }
+        },
+        data : { pageTitle : "Login" }
+    })
+    .state('register', {
+            url:'/register',
             views: {
                 'main': {
-                    templateUrl:'account/login.tpl.html',
-                    controller: 'LoginCtrl'
+                    templateUrl:'account/register.tpl.html',
+                    controller: 'RegisterCtrl'
                 }
             },
-            data : { pageTitle : "Login" }
-        })
-            .state('register', {
-                url:'/register',
-                views: {
-                    'main': {
-                        templateUrl:'account/register.tpl.html',
-                        controller: 'RegisterCtrl'
-                    }
-                },
-                data : { pageTitle : "Registration" }
+            data : { pageTitle : "Registration" }
             }
-        );
-    })
-    .factory('sessionService',function(){
-        var session = {};
-        session.login = function (data){
-            alert('user logged in with credentials: ' + data.name + ' and '+ data.password);
-            localStorage.setItem("session",data);
-        };
-        session.logout = function(){
-            localStorage.removeItem("session");
-        };
-        session.isLoggedIn = function(){
-            return localStorage.getItem("session") !== null;
-        };
-
-        return session;
-
-    })
-    .factory('accountService', function($resource){
-        var service = {};
-        service.register = function(account, success, failure){
-            var Account = $resource("/employee-portal/rest/accounts");
-            Account.save({}, account, success, failure);
-        };
-
-        service.userExists = function(account, success, failure){
-            var Account = $resource("/employee-portal/rest/accounts");
-            var data = Account.get({name:account.name}, function(){
-                    var accounts = data.accounts;
-                    if (accounts.length !== 0){
-                        success(accounts[0]);
-                    }else {
-                        failure();
-                    }
-
+    )
+    .state('accountSearch', {
+            url:'/accounts/search',
+            views: {
+                'main': {
+                    templateUrl:'account/search.tpl.html',
+                    controller: 'AccountSearchCtrl'
+                }
             },
-            failure);
-        };
-
-        return service;
-    })
-    .controller("LoginCtrl", function($scope, sessionService, accountService, $state) {
-        $scope.login = function() {
-            accountService.userExists($scope.account, function(account){
-                sessionService.login(account);
-                $state.go("home");
-            },
-            function(){
-             alert("Error logging the user");
-            });
-        };
-    })
-    .controller("RegisterCtrl", function($scope, sessionService, $state, accountService) {
-        $scope.register = function() {
-            accountService.register($scope.account,
-            function(returnedData){
-                sessionService.login(returnedData);
-                $state.go("home");
-            },
-            function(){
-                alert("Error registering the user");
-            });
-        };
+            data : { pageTitle : "Search Accounts" },
+            resolve: {
+                accounts: function(accountService) {
+                    return accountService.getAllAccounts();
+                }
+            }
     });
+})
+.factory('sessionService', function($http, $base64) {
+    var session = {};
+    session.login = function(data) {
+        alert('user logged in with credentials ' + data.name + " and " + data.password);
+        localStorage.setItem("session", data);
+    };
+    session.logout = function() {
+        localStorage.removeItem("session");
+    };
+    session.isLoggedIn = function() {
+        return localStorage.getItem("session") !== null;
+    };
+    return session;
+})
+.factory('blogService', function($resource) {
+    var service = {};
+    return service;
+})
+.factory('accountService', function($resource) {
+    var service = {};
+    service.register = function(account, success, failure) {
+        var Account = $resource("/employee-portal/rest/accounts");
+        Account.save({}, account, success, failure);
+    };
+    service.getAccountById = function(accountId) {
+        var Account = $resource("/employee-portal/rest/accounts/:paramAccountId");
+        return Account.get({paramAccountId:accountId}).$promise;
+    };
+    service.userExists = function(account, success, failure) {
+        var Account = $resource("/employee-portal/rest/accounts");
+        var data = Account.get({name:account.name, password:account.password}, function() {
+            var accounts = data.accounts;
+            if(accounts.length !== 0) {
+                success(account);
+            } else {
+                failure();
+            }
+        },
+        failure);
+    };
+    service.getAllAccounts = function() {
+          var Account = $resource("/employee-portal/rest/accounts");
+          return Account.get().$promise.then(function(data) {
+            return data.accounts;
+          });
+      };
+    return service;
+})
+.controller("LoginCtrl", function($scope, sessionService, accountService, $state) {
+    $scope.login = function() {
+        accountService.userExists($scope.account, function(account) {
+            sessionService.login($scope.account);
+            $state.go("home");
+        },
+        function() {
+            alert("Error logging in user");
+        });
+    };
+})
+.controller("RegisterCtrl", function($scope, sessionService, $state, accountService) {
+    $scope.register = function() {
+        accountService.register($scope.account,
+        function(returnedData) {
+            sessionService.login($scope.account);
+            $state.go("home");
+        },
+        function() {
+            alert("Error registering user");
+        });
+    };
+})
+.controller("AccountSearchCtrl", function($scope, accounts) {
+    $scope.accounts = accounts;
+});
